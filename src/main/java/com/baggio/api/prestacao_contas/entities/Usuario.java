3d800b91bb01138b8c96c819893a07c3fd269a10
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2026-05-08 17:06:50 -0300

    feat: add Usuario and UsuarioDTO

---
+++ src/main/java/com/baggio/api/prestacao_contas/entities/Usuario.java
@@ -0,0 +1,42 @@
+package com.baggio.api.prestacao_contas.entities;
+
+import java.time.LocalDateTime;
+
+import com.baggio.api.prestacao_contas.enums.Perfil;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Table(name = "usuarios")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  
+  private String nome;
+  private String email;
+  private String senha;
+  
+  @Enumerated(EnumType.STRING)
+  private Perfil perfil;
+  
+  private Boolean ativo;
+
+  @Column(name = "created_at")
+  private LocalDateTime createdAt;
+
+  @Column(name = "updated_at")
+  private LocalDateTime updatedAt;
+}
